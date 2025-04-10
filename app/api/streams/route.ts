2a18aec2ee06cabd77a7c3ae926040c62c8e2e0f
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-04-11 02:55:47 +0530

    build

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 import prismaClient from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import zod from 'zod';   
