c7d19a3fb6bbc99d52536d16cacaeba3d661a750
Sayandeep Dey <sayandeepdeyimp@gmail.com>
2025-04-11 02:22:58 +0530

    creator/creatorId

--- app/creator/[creatorId]/page.tsx
+++ app/creator/[creatorId]/page.tsx
@@ -1,15 +1,7 @@
 import Streamview from "@/app/components/StreamView";
 
-type Props = {
-  params: {
-    creatorId: string;
-  };
-};
+export default function CreatorPage({ params }: any) {
+  const { creatorId } = params;
 
-export default function CreatorPage({ params }: Props) {
-  return (
-    <div>
-      <Streamview creatorId={params.creatorId} />
-    </div>
-  );
+  return <Streamview creatorId={creatorId} />;
 }
